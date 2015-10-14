8aec6d19f4dc56ef0622c899d00970675f5a74f2
amanjpro <amanj.sherwany@usi.ch>
2015-10-14 16:40:57 +0200

    Add a continue related error code

--- brokenj/src/main/scala/errors/errorcodes.scala
+++ brokenj/src/main/scala/errors/errorcodes.scala
@@ -9,6 +9,14 @@ trait ErrorCodes extends primj.errors.ErrorCodes {
   case object DOUBLE_LABEL_DEF extends ErrorCode {
     val message: String = "Label is already defined"
   }
+
+  case object NO_LABEL_DEF extends ErrorCode {
+    val message: String = "Label not found"
+  }
+
+  case object BAD_CONTINUE_STMT extends ErrorCode {
+    val message: String = "Continue can only appear in iterative statements"
+  }
 }
 
 object ErrorCodes extends ErrorCodes
