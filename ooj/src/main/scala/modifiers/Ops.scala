22bfc6abe7733278bbf73227a059272df0d9cd23
amanjpro <amanj.sherwany@usi.ch>
2015-11-18 15:12:19 +0100

    Add flags for ooj

---
+++ ooj/src/main/scala/modifiers/Ops.scala
@@ -0,0 +1,27 @@
+package ch.usi.inf.l3.sana.ooj.modifiers
+
+
+
+import ch.usi.inf.l3.sana
+import sana.tiny
+import sana.primj
+import tiny.modifiers.Flags
+
+object Ops {
+
+  val noflags = tiny.modifiers.Ops.noflags
+
+
+  implicit class FlagOps(mask: Flags)
+      extends primj.modifiers.Ops.FlagOps(mask) {
+    val isConstructor:   Boolean       = mask.hasFlag(CONSTRUCTOR)
+    val isInterface: Boolean           = mask.hasFlag(INTERFACE)
+    val isStatic: Boolean              = mask.hasFlag(STATIC)
+    val isAbstract: Boolean            = mask.hasFlag(ABSTRACT)
+
+    val isPacakgeAcc: Boolean          = mask.hasFlag(PACKAGE_ACC)
+    val isPrivateAcc: Boolean          = mask.hasFlag(PRIVATE_ACC)
+    val isPublicAcc: Boolean           = mask.hasFlag(PUBLIC_ACC)
+    val isProtectedAcc: Boolean        = mask.hasFlag(PROTECTED_ACC)
+  }
+}
---
+++ ooj/src/main/scala/modifiers/flags.scala
@@ -0,0 +1,15 @@
+package ch.usi.inf.l3.sana.ooj.modifiers
+
+import ch.usi.inf.l3.sana.tiny.modifiers.Flag
+
+
+
+case object CONSTRUCTOR extends Flag
+case object STATIC extends Flag
+case object INTERFACE extends Flag
+case object ABSTRACT extends Flag
+
+case object PACKAGE_ACC extends Flag
+case object PUBLIC_ACC extends Flag
+case object PRIVATE_ACC extends Flag
+case object PROTECTED_ACC extends Flag
