066c879e1c30cabdba9ec19f99f5bac73359799c
amanjpro <amanj.sherwany@usi.ch>
2015-11-18 17:13:32 +0100

    Add typer for ``this''

--- ooj/src/main/scala/typechecker/typers.scala
+++ ooj/src/main/scala/typechecker/typers.scala
@@ -11,7 +11,6 @@ import sana.calcj
 import sana.dsl._
 import tiny.ast.{TreeCopiers => _, _}
 import tiny.types._
-import tiny.ast.Implicits._
 import tiny.types.TypeUtils._
 import tiny.symbols.{TypeSymbol, TermSymbol}
 import tiny.source.Position
@@ -19,23 +18,24 @@ import tiny.errors.ErrorReporting.{error,warning}
 import calcj.typechecker.{TyperComponent, TypePromotions}
 import calcj.types._
 // import primj.symbols._
-import primj.errors.ErrorCodes._
 import primj.types._
-import primj.modifiers.Ops._
+import ooj.modifiers.Ops._
 import ooj.ast._
 import ooj.symbols._
 import ooj.types._
+import ooj.ast.Implicits._
+import ooj.errors.ErrorCodes._
 
 /*
 CompilationUnit: DONE
 PackageDef: DONE
-ClassDef:
-Template:
+ClassDef: DONE
+Template: DONE
+MethodDef: DONE
 New:
 Select:
 This:
 Super:
-MethodDef:
 TypeUse:
 Ident:
 Apply:
@@ -67,21 +67,79 @@ trait ClassDefTyperComponent extends TyperComponent {
     val parents =
       clazz.parents.map((parent) => typed(parent).asInstanceOf[UseTree])
     val body    = typed(clazz.body).asInstanceOf[TemplateApi]
-    clazz.symbol match {
-      case Some(csym: ClassSymbol) =>
-        val qname   = packageName(csym)
-        val name    = csym.name
-        val psyms   = parents.flatMap(_.symbol).toSet
-        val tpe     = ClassType(qname, name, psyms)
-        clazz.symbol.foreach(_.tpe = Some(tpe))
-        clazz.tpe = tpe
-      case _                       =>
-        ()
-    }
     TreeCopiers.copyClassDef(clazz)(body = body, parents = parents)
   }
 
-
   protected def packageName(symbol: ClassSymbol): String =
     SymbolUtils.packageName(symbol)
+}
+
+@component
+trait TemplateTyperComponent extends TyperComponent {
+  (tmpl: TemplateApi) => {
+    val members = tmpl.members.map(typed(_).asInstanceOf[DefTree])
+    TreeCopiers.copyTemplate(tmpl)(members = members)
+  }
+}
+
+
+// TODO: Do we need this?
+@component
+trait MethodDefTyperComponent
+  extends primj.typechecker.MethodDefTyperComponent {
+  (mthd: MethodDefApi)             => {
+    super.apply(mthd)
+  }
+  // (mthd: MethodDefApi)          => {
+  //   val tpt     = typed(mthd.ret).asInstanceOf[UseTree]
+  //   val params  = mthd.params.map(typed(_).asInstanceOf[ValDefApi])
+  //   val body    = typed(mthd.body).asInstanceOf[Expr]
+  //   val tparams = params.map(_.tpe.getOrElse(ErrorType))
+  //   val rtpe    = tpt.tpe.getOrElse(ErrorType)
+  //   val btpe    = body.tpe.getOrElse(ErrorType)
+  //   if(!(btpe <:< rtpe) && rtpe =/= VoidType) {
+  //     error(TYPE_MISMATCH,
+  //         rtpe.toString, btpe.toString, body.pos, mthd)
+  //     mthd
+  //   } else {
+  //     // TODO: Check if all paths eventually return
+  //     if(rtpe =/= VoidType && !allPathsReturn(body)) {
+  //       error(MISSING_RETURN_STATEMENT,
+  //         body.toString, body.toString, body.pos, mthd)
+  //       mthd
+  //     } else {
+  //       TreeCopiers.copyMethodDef(mthd)(ret = tpt,
+  //         params = params, body = body)
+  //     }
+  //   }
+  // }
+
+
+  // def allPathsReturn(expr: Tree): Boolean = TreeUtils.allPathsReturn(expr)
+}
+
+
+@component
+trait ThisTyperComponent extends TyperComponent {
+  (ths: This)                 => {
+    val enclClass = ths.enclosingClassSymbol
+    val owner     = ths.owner
+    enclClass match {
+      case None                  =>
+        error(ACCESSING_THIS_OUTSIDE_A_CLASS,
+              ths.toString, "", ths.pos, ths)
+      case _                     =>
+        ()
+    }
+
+    owner.foreach { sym => sym.mods.isStatic match {
+        case true                  =>
+          error(ACCESSING_THIS_IN_STATIC,
+                ths.toString, "", ths.pos, ths)
+        case false                 =>
+          ()
+      }
+    }
+    ths
+  }
 }
