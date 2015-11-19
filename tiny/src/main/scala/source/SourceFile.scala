fd0627fefed22ea344592ce127c7bb2ebb35b2a0
amanjpro <amanj.sherwany@usi.ch>
2015-11-19 17:28:25 +0100

    Add helper methods for source name and path

--- tiny/src/main/scala/source/SourceFile.scala
+++ tiny/src/main/scala/source/SourceFile.scala
@@ -2,5 +2,20 @@ package ch.usi.inf.l3.sana.tiny.source
 
 import org.antlr.v4.runtime.tree.ParseTree
 
-case class SourceFile(name: String, content: ParseTree)
+case class SourceFile(name: String, content: ParseTree) {
+  private[this] lazy val files: List[String] =
+    name.split("[.]").reverse.toList match {
+      case "java"::xs         => xs
+      case xs                 => xs
+    }
+  def fileName: String = files match {
+    case x::_          => x
+    case _             => ""
+  }
+
+  def filePath: List[String] = files match {
+    case _::xs         => xs
+    case _             => Nil
+  }
+}
 
