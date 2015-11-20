80a4daf462a72af55cfcf841db801300b0726ab1
amanjpro <amanj.sherwany@usi.ch>
2015-11-20 11:34:37 +0100

    Add OojNodes object

---
+++ ooj/src/main/scala/OojNodes.scala
@@ -0,0 +1,37 @@
+package ch.usi.inf.l3.sana.ooj
+
+
+object OojNodes {
+  final val nodes =
+    """CompilationUnit,
+       PackageDef,
+       ClassDef,
+       Template,
+       MethodDef,
+       Select,
+       This,
+       Super,
+       New,
+       Label,
+       Swtich,
+       Case,
+       Break,
+       Continue,
+       ValDef,
+       Assign,
+       If,
+       While,
+       Block,
+       For,
+       Ternary,
+       Apply,
+       Return,
+       Cast,
+       Binary,
+       Unary,
+       Literal,
+       TypeUse,
+       Ident"""
+
+
+}
