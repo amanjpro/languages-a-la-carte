1d9f7ed632fbf5374872389dbc4040f8a1559ba3
amanjpro <amanj.sherwany@usi.ch>
2015-12-08 13:43:57 +0100

    Add a typetyper family

---
+++ ooj/src/main/scala/phases/TypeTyperFamily.scala
@@ -0,0 +1,28 @@
+package ch.usi.inf.l3.sana.ooj.phases
+
+import ch.usi.inf.l3.sana
+import sana.dsl._
+import sana.core._
+import sana.core.Implicits._
+import sana.tiny.ast.{Tree, NoTree}
+import sana.ooj.OojNodes
+import sana.ooj.typechecker._
+
+
+
+trait TypeTyperFamily extends TransformationFamily[Tree, Tree] {
+  self =>
+
+  override def default: Tree = NoTree
+
+  def components: List[PartialFunction[Tree, Tree]] =
+    generateComponents[Tree, Tree]("ClassDef,PackageDef,CompilationUnit",
+      "TypeTyperComponent", "typed", "")
+
+  def typed: Tree => Tree = family
+}
+
+object TypeTyperFamily extends TypeTyperFamily
+
+
+
