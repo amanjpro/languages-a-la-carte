922b2c33421d3ee7fb8ed396c4614eb9ad42fd74
amanjpro <amanj.sherwany@usi.ch>
2015-12-08 15:04:32 +0100

    Add methods for supporting method overloading

--- ooj/src/main/scala/symbols/SymbolUtils.scala
+++ ooj/src/main/scala/symbols/SymbolUtils.scala
@@ -68,6 +68,74 @@ trait SymbolUtils extends sana.primj.symbols.SymbolUtils {
     langPackageSymbol.getSymbol(name, _ => true).get.asInstanceOf[ClassSymbol]
   }
 
+
+  // Method Overloading
+  /**
+   * Taking a list of method id and method type pairs, finds the most
+   * specific methods.
+   *
+   * According to Java's spec (1.0 ed) a method is more specific than the
+   * other, if all its parameters can be passed to the latter.
+   */
+  def mostSpecificMethods(symbols: List[Symbol]): List[Symbol] = {
+    symbols.filter{ sym =>
+      symbols.foldLeft(true)((z, y) => {
+        if(sym == y) z
+        else {
+          (sym, y) match {
+            case (m1: MethodSymbol, m2: MethodSymbol) =>
+              methodCanBeApplied(m1.params.flatMap(_.tpe), m2.params.flatMap(_.tpe))
+            case _                                    =>
+              false
+          }
+        }
+      })
+    }
+  }
+
+
+  def methodCanBeApplied(ptpes: List[Type],
+                           atpes: List[Type]): Boolean = {
+    if(ptpes.size != atpes.size) false
+    else
+      atpes.zip(ptpes).foldLeft(true)((z, y) => {
+        z && (y._1 <:< y._2)
+      })
+  }
+
+
+  def isAccessible(symbol: Symbol, from: Symbol): Boolean = {
+    def areInTheSamePackages(sym1: Symbol, sym2: Symbol): Boolean = {
+      val r = for {
+        s1 <- enclosingPackage(Some(sym1))
+        s2 <- enclosingPackage(Some(sym2))
+      } yield s1 == s2
+      r.getOrElse(false)
+    }
+
+    if(symbol.mods.isPublicAcc) true
+    else if(symbol.mods.isPrivateAcc) {
+      val r = for {
+        s1 <- enclosingClass(Some(symbol))
+        s2 <- enclosingClass(Some(from))
+      } yield {
+        if(s1 == s2) true
+        else isAccessible(symbol, s2) // In case symbol is an inner class
+      }
+      r.getOrElse(false)
+    } else if(symbol.mods.isProtectedAcc) {
+      lazy val r = for {
+        s1 <- enclosingClass(Some(symbol))
+        s2 <- enclosingClass(Some(from))
+        t1 <- s1.tpe
+        t2 <- s2.tpe
+      } yield {
+        if(t2 <:< t1 || s1 == s2) true
+        else isAccessible(symbol, s2) // In case symbol is an inner class
+      }
+      areInTheSamePackages(symbol, from) || r.getOrElse(false)
+    } else areInTheSamePackages(symbol, from)
+  }
 }
 
 object SymbolUtils extends SymbolUtils
