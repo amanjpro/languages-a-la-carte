2163c9a6911e26f22568d0490e7bce29027786c7
amanjpro <amanj.sherwany@usi.ch>
2016-01-07 19:34:51 +0100

    Add a way to check if a local-var/param is already defined

--- primj/src/main/scala/symbols/SymbolUtils.scala
+++ primj/src/main/scala/symbols/SymbolUtils.scala
@@ -3,6 +3,7 @@ package ch.usi.inf.l3.sana.primj.symbols
 import ch.usi.inf.l3.sana
 import sana.tiny.symbols.Symbol
 import sana.tiny.types.Type
+import sana.tiny.names.Name
 import sana.primj.types.VoidType
 
 trait SymbolUtils extends sana.calcj.symbols.SymbolUtils {
@@ -19,6 +20,20 @@ trait SymbolUtils extends sana.calcj.symbols.SymbolUtils {
       case sym: MethodSymbol => Some(sym)
       case sym               => enclosingMethod(sym.owner)
     }
+
+
+  def alreadyDefinedLocalVarable(owner: Option[Symbol],
+    name: Name): Boolean = {
+    owner match {
+      case Some(owner: MethodSymbol)          =>
+        owner.directlyDefinesName(name, _.isInstanceOf[VariableSymbol])
+      case Some(owner: ScopeSymbol)           =>
+        owner.directlyDefinesName(name, _.isInstanceOf[VariableSymbol]) ||
+          alreadyDefinedLocalVarable(owner.owner, name)
+      case _                                  =>
+        false
+    }
+  }
 
 }
 
