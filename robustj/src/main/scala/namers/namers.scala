789b724e4d333a8fde0a70f71ffbf10e732229d5
amanjpro <amanj.sherwany@usi.ch>
2016-03-10 22:26:08 +0100

    Add namers for robustj

---
+++ robustj/src/main/scala/namers/namers.scala
@@ -0,0 +1,32 @@
+package ch.usi.inf.l3.sana.robustj.namers
+
+import ch.usi.inf.l3.sana
+import sana.primj
+import sana.tiny
+import sana.calcj
+import sana.brokenj
+import sana.arrayj
+import sana.arrooj
+import sana.ooj
+import sana.robustj
+
+import sana.core.TransformationComponent
+import sana.dsl._
+import robustj.ast.{TreeFactories, MethodDefApi}
+import tiny.ast.UseTree
+
+@component
+trait MethodDefNamerComponent extends
+    ooj.namers.MethodDefNamerComponent {
+  (mthd: MethodDefApi) => {
+    val res1 = super.apply(mthd).asInstanceOf[ooj.ast.MethodDefApi]
+    val throwsClause = mthd.throwsClause.map { tc =>
+      name(tc).asInstanceOf[UseTree]
+    }
+    // INFO: a bit of hack, but works
+    val res2 = TreeFactories.mkMethodDef(res1.mods, res1.ret, res1.name,
+      res1.params, throwsClause, res1.body)
+    res2.attributes = res1.attributes
+    res2
+  }
+}
