6644b3c54df1b9b4e738574963360da394f7c60c
amanjpro <amanj.sherwany@usi.ch>
2016-03-11 11:07:55 +0100

    Avoid using path-dependent types

--- dynj/src/main/scala/ast/operators.scala
+++ dynj/src/main/scala/ast/operators.scala
@@ -4,7 +4,7 @@ package ch.usi.inf.l3.sana.dynj.ast
 import ch.usi.inf.l3.sana.calcj
 import calcj.ast.operators.BOp
 
-trait operators extends calcj.ast.operators {
+trait operators {
   object IsInstanceOf extends BOp {
     override def toString: String = "isinstanceof"
   }
