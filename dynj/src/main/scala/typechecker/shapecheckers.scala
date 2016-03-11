5e61d7c2160072d38e5f74cca139dba20a07eedd
amanjpro <amanj.sherwany@usi.ch>
2016-03-11 11:10:37 +0100

    Add shapechecker for dynj
    To check the well-shapedness for binary tree

---
+++ dynj/src/main/scala/typechecker/shapecheckers.scala
@@ -0,0 +1,50 @@
+package ch.usi.inf.l3.sana.dynj.typechecker
+
+
+import ch.usi.inf.l3.sana
+import sana.dynj
+import sana.robustj
+import sana.arrooj
+import sana.ooj
+import sana.arrayj
+import sana.brokenj
+import sana.primj
+import sana.tiny
+import sana.calcj
+
+
+import sana.dsl._
+import tiny.ast.Tree
+import calcj.ast.BinaryApi
+import tiny.errors.ErrorReporting.{error,warning}
+import primj.typechecker.ShapeCheckerComponent
+import tiny.ast.Implicits._
+import dynj.errors.ErrorCodes._
+import dynj.ast.operators.IsInstanceOf
+import robustj.ast.TreeUtils
+
+@component
+trait BinaryShapeCheckerComponent extends ShapeCheckerComponent {
+  (bin: BinaryApi)     => {
+    check(bin.lhs)
+    check(bin.rhs)
+    if(!isValidExpression(bin.lhs))
+      error(BAD_EXPRESSION, "", "", bin.lhs.pos)
+
+    bin.op match {
+      case IsInstanceOf if !isTypeUse(bin.rhs)         =>
+        error(BAD_EXPRESSION, "", "", bin.rhs.pos)
+      case _            if !isValidExpression(bin.rhs) =>
+        error(BAD_EXPRESSION, "", "", bin.rhs.pos)
+      case _                                           =>
+        ()
+    }
+  }
+
+
+  def isTypeUse(t: Tree): Boolean =
+    TreeUtils.isValidExpression(t)
+
+  protected def isValidExpression(t: Tree): Boolean =
+    TreeUtils.isValidExpression(t)
+}
