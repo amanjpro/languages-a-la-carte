0f436833c778a935dffbddf655771f2a5b945584
amanjpro <amanj.sherwany@usi.ch>
2016-03-11 11:10:00 +0100

    Add typers for dynj
    To check the correctness of isinstanceof

---
+++ dynj/src/main/scala/typechecker/typers.scala
@@ -0,0 +1,39 @@
+package ch.usi.inf.l3.sana.dynj.typechecker
+
+import ch.usi.inf.l3.sana
+import sana.tiny
+import sana.calcj
+import sana.ooj
+import sana.dynj
+
+
+import sana.dsl._
+
+import tiny.errors.ErrorReporting.{error,warning}
+import tiny.ast.Implicits._
+import tiny.types.Type
+import calcj.ast.BinaryApi
+import calcj.types.BooleanType
+import ooj.types.RefType
+import dynj.errors.ErrorCodes._
+import dynj.ast.operators.IsInstanceOf
+
+
+@component
+trait BinaryTyperComponent extends ooj.typechecker.BinaryTyperComponent {
+
+  override protected def binaryTyper(ltpe: Type,
+    rtpe: Type, bin: BinaryApi): Option[(Type, Type, Type)] = bin.op match {
+    case IsInstanceOf                              =>
+      if(ltpe.isInstanceOf[RefType] && rtpe.isInstanceOf[RefType]) {
+        Some((ltpe, rtpe, BooleanType))
+      } else {
+        error(DYNAMIC_TYPE_CHECK_NONE_REF_TYPE, "", "", bin.pos)
+        None
+      }
+    case _                                          =>
+      super.binaryTyper(ltpe, rtpe, bin)
+  }
+
+}
+
