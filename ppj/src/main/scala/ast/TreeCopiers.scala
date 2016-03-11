6b5d13611b0b068e6835154fbdbf26f1f41d5edf
amanjpro <amanj.sherwany@usi.ch>
2016-03-11 11:36:16 +0100

    Add copiers and factories for synchronized

---
+++ ppj/src/main/scala/ast/TreeCopiers.scala
@@ -0,0 +1,25 @@
+package ch.usi.inf.l3.sana.ppj.ast
+
+import ch.usi.inf.l3.sana
+import sana.tiny.types.Type
+import sana.tiny.source.Position
+import sana.tiny.symbols.Symbol
+import sana.tiny.ast.Implicits._
+import sana.tiny.ast.Expr
+import sana.primj.ast.BlockApi
+
+
+
+trait TreeCopiers extends sana.robustj.ast.TreeCopiers {
+  def copySynchronized(template: SynchronizedApi)(
+      expr: Expr = template.expr, block: BlockApi = template.block,
+      pos: Option[Position] = template.pos,
+      symbol: Option[Symbol] = template.symbol,
+      owner: Option[Symbol]  = template.owner): SynchronizedApi = {
+    val res = TreeFactories.mkSynchronized(expr, block)
+    copyProperties(template, res)
+    res
+  }
+}
+
+object TreeCopiers extends TreeCopiers
---
+++ ppj/src/main/scala/ast/TreeFactories.scala
@@ -0,0 +1,25 @@
+package ch.usi.inf.l3.sana.ppj.ast
+
+import ch.usi.inf.l3.sana
+import sana.tiny.types.Type
+import sana.tiny.source.Position
+import sana.tiny.ast.Implicits._
+import sana.tiny.symbols.Symbol
+import sana.tiny.ast.Expr
+import sana.primj.ast.BlockApi
+
+
+
+trait TreeFactories extends sana.robustj.ast.TreeFactories {
+  def mkSynchronized(expr: Expr, block: BlockApi,
+    pos: Option[Position] = None, symbol: Option[Symbol] = None,
+    owner: Option[Symbol] = None): SynchronizedApi = {
+    val res = new Synchronized(expr, block)
+    pos.foreach(res.pos = _)
+    owner.foreach(res.owner = _)
+    symbol.foreach(res.symbol = _)
+    res
+  }
+}
+
+object TreeFactories extends TreeFactories
