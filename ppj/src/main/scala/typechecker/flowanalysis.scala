47bd118d242faa0ea6c8eb96d88b33abd0d6442d
amanjpro <amanj.sherwany@usi.ch>
2016-03-11 14:47:27 +0100

    Add flowanalysis for synchronized blocks

---
+++ ppj/src/main/scala/typechecker/flowanalysis.scala
@@ -0,0 +1,32 @@
+package ch.usi.inf.l3.sana.ppj.typechecker
+
+import ch.usi.inf.l3.sana
+import sana.primj
+import sana.tiny
+import sana.calcj
+import sana.brokenj
+import sana.arrayj
+import sana.ooj
+import sana.ppj
+import sana.robustj
+
+import sana.dsl._
+import tiny.ast.{Tree, Expr}
+import tiny.errors.ErrorReporting.{error,warning}
+import tiny.errors.ErrorCodes._
+import tiny.ast.Implicits._
+import primj.ast.BlockApi
+import ooj.typechecker.FlowCorrectnessCheckerComponent
+import ppj.ast.TreeUtils
+import ppj.ast._
+
+
+@component(tree, env)
+trait SynchronizedFlowCorrectnessCheckerComponent
+  extends FlowCorrectnessCheckerComponent {
+  (sync: SynchronizedApi)     => {
+    val r1 = check((sync.expr, env))
+    val r2 = check((sync.block, env))
+    r1.unify(r2)
+  }
+}
