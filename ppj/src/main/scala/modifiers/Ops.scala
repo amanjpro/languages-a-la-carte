61ee8da83fdf19476c4e6b1292cb3aacdc63e830
amanjpro <amanj.sherwany@usi.ch>
2016-03-11 11:41:30 +0100

    Add SYNCHRONIZED flag

---
+++ ppj/src/main/scala/modifiers/Ops.scala
@@ -0,0 +1,19 @@
+package ch.usi.inf.l3.sana.ppj.modifiers
+
+
+
+import ch.usi.inf.l3.sana
+import sana.tiny
+import sana.robustj
+import tiny.modifiers.Flags
+
+object Ops {
+
+  val noflags = tiny.modifiers.Ops.noflags
+
+
+  implicit class FlagOps(mask: Flags)
+      extends robustj.modifiers.Ops.FlagOps(mask) {
+    val isSynchronized: Boolean            = mask.hasFlag(SYNCHRONIZED)
+  }
+}
---
+++ ppj/src/main/scala/modifiers/flags.scala
@@ -0,0 +1,8 @@
+package ch.usi.inf.l3.sana.ppj.modifiers
+
+import ch.usi.inf.l3.sana.tiny.modifiers.Flag
+
+
+
+
+case object SYNCHRONIZED extends Flag
