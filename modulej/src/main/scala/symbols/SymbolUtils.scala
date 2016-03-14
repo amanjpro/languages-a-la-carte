dbde36950f9e241b011aa0071698cb256d60457a
amanjpro <amanj.sherwany@usi.ch>
2016-03-14 14:47:14 +0100

    Add SymbolUtils.fulyQualifiedSymbolList(PackageSymbol) method

---
+++ modulej/src/main/scala/symbols/SymbolUtils.scala
@@ -0,0 +1,30 @@
+package ch.usi.inf.l3.sana.modulej.symbols
+
+import ch.usi.inf.l3.sana
+import sana.tiny.symbols.Symbol
+import sana.tiny.types.Type
+import sana.tiny.names.Name
+import sana.calcj.types.IntType
+import sana.ooj.symbols.ClassSymbol
+import sana.ooj.modifiers._
+import sana.robustj.names.StdNames
+
+import sana.ooj.symbols.PackageSymbol
+
+trait SymbolUtils extends sana.robustj.symbols.SymbolUtils {
+  /**
+   * Return the fully qualified symbols for all qualified packages of
+   * this symbol. The top-most package is the head of the list.
+   *
+   * The result is guaranteed to be none-empty
+   */
+  def fullyQualifiedSymbolList(pkg: PackageSymbol): List[PackageSymbol] =
+    pkg.owner match {
+      case Some(ow: PackageSymbol)    =>
+        fullyQualifiedSymbolList(ow) ++ List(pkg)
+      case _                          => List(pkg)
+    }
+}
+
+object SymbolUtils extends SymbolUtils
+
