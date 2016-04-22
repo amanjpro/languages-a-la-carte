b76e520906ea9a3149375e5c41e05eddc280c5d2
amanjpro <amanj.sherwany@usi.ch>
2016-04-22 23:30:46 +0200

    Add more utility functions

--- modulej/src/main/scala/ast/TreeUtils.scala
+++ modulej/src/main/scala/ast/TreeUtils.scala
@@ -16,8 +16,11 @@ import sana.modulej
 
 
 
-import tiny.ast.UseTree
-import tiny.ast.Tree
+import tiny.ast.{SimpleUseTree, UseTree, Tree}
+import modulej.ast.Implicits._
+import ooj.ast.PackageDefApi
+import ooj.names.StdNames.DEFAULT_PACKAGE_NAME
+import modulej.ast.TreeExtractors._
 import tiny.names.{Name, StdNames}
 import tiny.source.Position
 import ooj.symbols.PackageSymbol
@@ -38,6 +41,72 @@ trait TreeUtils extends ppj.ast.TreeUtils {
     }
     toQualifiedTree(sym.qualifiedNameAsList.reverse)
   }
+
+  def fromQualifiedString(name: String): UseTree = {
+    def helper(names: List[String]): UseTree = names match {
+      case (n::Nil)                          =>
+        TreeFactories.mkIdent(Name(n))
+      case (n::ns)                           =>
+        val tree = TreeFactories.mkIdent(Name(n))
+        val qual = helper(ns)
+        TreeFactories.mkSelect(qual, tree)
+      case _                                 =>
+        ???
+    }
+    helper(name.split("[.]").toList.reverse)
+  }
+
+  def toQualifiedString(use: UseTree): String = use match {
+    case Select(qual: UseTree, t)           =>
+      s"${toQualifiedString(qual)}.${t.name.asString}"
+    case Ident(name)                        =>
+      name.asString
+    case TypeUse(name)                      =>
+      name.asString
+    case _                                  =>
+      ""
+  }
+
+  def attachQualifiedNameAttribute(use: UseTree): Unit = {
+    def helper(use: UseTree): Unit = {
+      use match {
+        case s: SimpleUseTree                =>
+          s.fullyQualifiedName = s.name.asString
+        case Select(qual: UseTree, tree)     =>
+          helper(qual)
+          qual.fullyQualifiedName match {
+            case None                        =>
+              use.fullyQualifiedName      = tree.name.asString
+              tree.fullyQualifiedName     = tree.name.asString
+            case Some(n)                     =>
+              use.fullyQualifiedName      = s"$n.${tree.name.asString}"
+              tree.fullyQualifiedName     = s"$n.${tree.name.asString}"
+          }
+        case _                               =>
+          ()
+      }
+    }
+    use.fullyQualifiedName match {
+      case None                  => helper(use)
+      case Some(fq)              => ()
+    }
+  }
+
+  /** Takes a list of package names and turns them into a hierarchy of
+    * packages. The head of the list is the outer most package,
+    * and the tail is the inner ones.
+    */
+  def toPackage(names: List[String]): PackageDefApi =
+    toPackageAux(names.reverse)
+
+  protected def toPackageAux(names: List[String]): PackageDefApi =
+    names match {
+      case (x::xs)                  =>
+        TreeFactories.mkPackageDef(xs.map(Name(_)).reverse, Name(x), Nil)
+      case _                         =>
+        TreeFactories.mkPackageDef(Nil, DEFAULT_PACKAGE_NAME, Nil)
+    }
 }
 
 object TreeUtils extends TreeUtils
+
