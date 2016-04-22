c3bf9469801ab4de4eca8d81d268eef1c076fd49
amanjpro <amanj.sherwany@usi.ch>
2016-04-22 23:32:12 +0200

    Add Nodes and Main classes to modulej

---
+++ modulej/src/main/scala/Nodes.scala
@@ -0,0 +1,47 @@
+package ch.usi.inf.l3.sana.modulej
+
+
+object Nodes {
+  final val nodes =
+    """Program,
+       CompilationUnit,
+       Import,
+       PackageDef,
+       ClassDef,
+       Template,
+       MethodDef,
+       Throw,
+       Try,
+       Catch,
+       Select,
+       This,
+       Super,
+       New,
+       Synchronized,
+       ArrayCreation,
+       ArrayAccess,
+       ArrayInitializer,
+       ArrayTypeUse,
+       Label,
+       Switch,
+       Case,
+       Break,
+       Continue,
+       ValDef,
+       Assign,
+       If,
+       While,
+       Block,
+       For,
+       Ternary,
+       Apply,
+       Return,
+       Cast,
+       Binary,
+       Unary,
+       Literal,
+       TypeUse,
+       Ident"""
+
+
+}
