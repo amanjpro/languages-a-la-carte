8a2e26c122e8fd7b55d96a86a228876e3b61d434
amanjpro <amanj.sherwany@usi.ch>
2016-04-22 23:36:04 +0200

    Add constant-folding related phases to modulej

---
+++ modulej/src/main/scala/eval/constantcollectors.scala
@@ -0,0 +1,27 @@
+package ch.usi.inf.l3.sana.modulej.eval
+
+
+import ch.usi.inf.l3.sana
+import sana.ooj
+import sana.modulej
+import sana.tiny
+
+import tiny.core.TransformationComponent
+import tiny.dsl._
+
+
+import modulej.ast._
+import ooj.ast.PackageDefApi
+import ooj.eval.ConstantCollectingComponent
+
+
+
+@component(tree, env)
+trait CompilationUnitConstantCollectingComponent extends
+    ConstantCollectingComponent {
+  (cunit: CompilationUnitApi)  => {
+    val (module, newEnv) = collect((cunit.module, env))
+    (TreeCopiers.copyCompilationUnit(cunit)(module =
+        module.asInstanceOf[PackageDefApi]), newEnv)
+  }
+}
---
+++ modulej/src/main/scala/eval/constantfolding.scala
@@ -0,0 +1,69 @@
+package ch.usi.inf.l3.sana.modulej.eval
+
+
+import ch.usi.inf.l3.sana
+import sana.ooj
+import sana.modulej
+import sana.tiny
+
+import tiny.core.{TransformationComponent, CompilerInterface}
+import tiny.dsl._
+
+
+import modulej.ast._
+import modulej.ast.Implicits._
+import ooj.ast.PackageDefApi
+import tiny.ast.{UseTree, IdentApi, TypeUseApi}
+import ooj.eval.ConstantFoldingComponent
+
+
+
+
+@component(tree, env)
+trait CompilationUnitConstantFoldingComponent extends
+    ConstantFoldingComponent {
+  (cunit: CompilationUnitApi)  => {
+    val (module, newEnv) = constantFold((cunit.module, env))
+    (TreeCopiers.copyCompilationUnit(cunit)(module =
+        module.asInstanceOf[PackageDefApi]), newEnv)
+  }
+}
+
+@component(tree, env)
+trait TypeUseConstantFoldingComponent
+  extends ooj.eval.TypeUseConstantFoldingComponent {
+
+  override protected def nameTypeUse(tuse: TypeUseApi): UseTree =
+    typeUseNamer.nameTypeUse(tuse)
+
+  private[this] val typeUseNamer = {
+    val comp = this
+    new modulej.namers.TypeUseNamer {
+      protected val compiler: CompilerInterface = comp.compiler
+      def family(use: UseTree): UseTree = compiler.typeCheck(
+        use.owner)(use).asInstanceOf[UseTree]
+    }
+  }
+
+}
+
+@component(tree, env)
+trait IdentConstantFoldingComponent
+  extends ooj.eval.IdentConstantFoldingComponent {
+
+  override protected def nameIdent(id: IdentApi): UseTree =
+    identNamer.nameIdent(id)
+
+  override protected def typeAndNameIdent(id: IdentApi): UseTree =
+    identNamer.nameIdent(id, false)
+
+  private[this] val identNamer = {
+    val comp = this
+    new modulej.namers.IdentNamer with ooj.typechecker.IdentNamer {
+      protected val compiler: CompilerInterface = comp.compiler
+      def family(use: UseTree): UseTree = compiler.typeCheck(
+        use.owner)(use).asInstanceOf[UseTree]
+    }
+  }
+
+}
