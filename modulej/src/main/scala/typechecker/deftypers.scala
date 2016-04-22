81726b594362462b598be520bce89a33e17f323f
amanjpro <amanj.sherwany@usi.ch>
2016-04-22 23:36:43 +0200

    Add typer and deftyper to modulej

---
+++ modulej/src/main/scala/typechecker/deftypers.scala
@@ -0,0 +1,33 @@
+package ch.usi.inf.l3.sana.modulej.typechecker
+
+import ch.usi.inf.l3.sana
+import sana.modulej
+import sana.ooj
+import sana.tiny
+
+import tiny.core.TransformationComponent
+import tiny.dsl._
+
+
+import modulej.ast._
+import ooj.ast.PackageDefApi
+import tiny.ast.UseTree
+import ooj.typechecker.DefTyperComponent
+
+
+@component
+trait ImportDefTyperComponent extends DefTyperComponent {
+  (imprt: ImportApi) => {
+    val qual = typed(imprt.qual).asInstanceOf[UseTree]
+    TreeCopiers.copyImport(imprt)(qual = qual)
+  }
+}
+
+@component
+trait CompilationUnitDefTyperComponent extends DefTyperComponent {
+  (unit: CompilationUnitApi) => {
+    val pkg     = typed(unit.module).asInstanceOf[PackageDefApi]
+    val imports = unit.imports.map(typed(_).asInstanceOf[ImportApi])
+    TreeCopiers.copyCompilationUnit(unit)(imports = imports, module = pkg)
+  }
+}
---
+++ modulej/src/main/scala/typechecker/typers.scala
@@ -0,0 +1,148 @@
+package ch.usi.inf.l3.sana.modulej.typechecker
+
+import ch.usi.inf.l3.sana
+import sana.modulej
+import sana.ooj
+import sana.calcj
+import sana.tiny
+import sana.ppj
+import sana.robustj
+
+import tiny.core.{CompilerInterface, TransformationComponent}
+import tiny.dsl._
+
+
+import modulej.ast._
+import modulej.ast.Implicits._
+import ooj.ast.{PackageDefApi, SelectApi}
+import tiny.ast.{IdentApi, TypeUseApi, UseTree, NoTree, Tree}
+import robustj.ast.{MethodDefApi}
+import tiny.errors.ErrorReporting.{error,warning}
+import modulej.errors.ErrorCodes._
+import modulej.modifiers.Ops._
+import calcj.typechecker.TyperComponent
+
+
+@component
+trait ImportTyperComponent extends TyperComponent {
+  (imprt: ImportApi) => {
+    val qual = typed(imprt.qual).asInstanceOf[UseTree]
+    qual.symbol match {
+      case None       if !imprt.isOnDemand                     =>
+        error(IMPORTED_PACKAGE_IS_MISSING,
+          "", "", qual.pos)
+      case None                                                =>
+        error(IMPORTED_CLASS_IS_MISSING,
+          "", "", qual.pos)
+      case _                                                   =>
+        ()
+    }
+    TreeCopiers.copyImport(imprt)(qual = qual)
+  }
+}
+
+@component
+trait CompilationUnitTyperComponent extends TyperComponent {
+  (unit: CompilationUnitApi) => {
+    val pkg     = typed(unit.module).asInstanceOf[PackageDefApi]
+    val imports = unit.imports.map(typed(_).asInstanceOf[ImportApi])
+    TreeCopiers.copyCompilationUnit(unit)(imports = imports, module = pkg)
+  }
+}
+
+
+@component
+trait IdentTyperComponent extends ooj.typechecker.IdentTyperComponent {
+  (id: IdentApi)          => {
+    attachQualifiedNameAttribute(id)
+    super.apply(id)
+  }
+  override protected def nameIdent(id: IdentApi): UseTree =
+    identNamer.nameIdent(id)
+
+  override protected def typeAndNameIdent(id: IdentApi): UseTree =
+    identNamer.nameIdent(id, true)
+
+
+  private[this] val identNamer = {
+    val comp = this
+    new modulej.namers.IdentNamer with ooj.typechecker.IdentNamer {
+      protected val compiler: CompilerInterface = comp.compiler
+      def family(use: UseTree): UseTree = comp.typed(use).asInstanceOf[UseTree]
+    }
+  }
+  protected def attachQualifiedNameAttribute(use: UseTree): Unit =
+    TreeUtils.attachQualifiedNameAttribute(use)
+}
+
+@component
+trait TypeUseTyperComponent
+  extends ooj.typechecker.TypeUseTyperComponent {
+  (tuse: TypeUseApi) => {
+    attachQualifiedNameAttribute(tuse)
+    val tuseCopy = if(!tuse.hasBeenNamed) {
+      typeUseNamer.nameTypeUse(tuse)
+    } else tuse
+    super.apply(tuseCopy)
+  }
+
+  override protected def nameTypeUse(tuse: TypeUseApi): UseTree =
+    typeUseNamer.nameTypeUse(tuse)
+
+  private[this] val typeUseNamer = {
+    val comp = this
+    new modulej.namers.TypeUseNamer {
+      protected val compiler: CompilerInterface = comp.compiler
+      def family(use: UseTree): UseTree =
+        comp.typed(use).asInstanceOf[UseTree]
+    }
+  }
+  protected def attachQualifiedNameAttribute(use: UseTree): Unit =
+    TreeUtils.attachQualifiedNameAttribute(use)
+}
+
+
+@component
+trait SelectTyperComponent extends ooj.typechecker.SelectTyperComponent {
+  (slct: SelectApi) => {
+    attachQualifiedNameAttribute(slct)
+    super.apply(slct)
+  }
+
+  protected def attachQualifiedNameAttribute(use: UseTree): Unit =
+    TreeUtils.attachQualifiedNameAttribute(use)
+}
+
+
+
+@component
+trait MethodDefTyperComponent extends
+  ppj.typechecker.MethodDefTyperComponent {
+
+  (mthd: MethodDefApi) => {
+    val mthd2 = typed(mthd).asInstanceOf[MethodDefApi]
+
+    if(mthd2.mods.isNative && !isConstructor(mthd.symbol) &&
+          mthd.body != NoTree) {
+      error(NATIVE_METHOD_CANNOT_HAVE_BODY,
+          "", "", mthd.pos)
+    }
+
+    if(mthd2.mods.isNative && isConstructor(mthd.symbol)) {
+      error(CONSTRUCTOR_CANNOT_BE_NATIVE,
+          "", "", mthd.pos)
+    }
+
+
+    mthd2
+  }
+
+  override def allPathsReturn(expr: Tree): Boolean = {
+    enclosingMethod(expr.symbol) match {
+      case Some(mthd)                         =>
+        mthd.mods.isFinal || allPathsReturn(expr)
+      case None                               =>
+        allPathsReturn(expr)
+    }
+  }
+}
