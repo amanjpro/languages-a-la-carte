b21c3abeb0dc9566c67acaf12f63f3ed6608245e
amanjpro <amanj.sherwany@usi.ch>
2016-04-28 19:28:13 +0200

    Clean-up toFullyQualifiedTree method

--- modulej/src/main/scala/ast/TreeUtils.scala
+++ modulej/src/main/scala/ast/TreeUtils.scala
@@ -28,18 +28,7 @@ import ooj.symbols.PackageSymbol
 trait TreeUtils extends ppj.ast.TreeUtils {
   def toFullyQualifiedTree(sym: PackageSymbol,
           pos: Option[Position]): UseTree = {
-    def toQualifiedTree(names: List[Name]): UseTree = names match {
-      case (x::Nil)                       =>
-        TreeFactories.mkIdent(x, pos)
-      case (x::xs)                        =>
-        val id   = TreeFactories.mkIdent(x, pos)
-        val qual = toQualifiedTree(xs)
-        TreeFactories.mkSelect(qual, id, pos)
-      case Nil                            =>
-        // INFO: This should never happen
-        TreeFactories.mkIdent(StdNames.noname, pos)
-    }
-    toQualifiedTree(sym.qualifiedNameAsList.reverse)
+    fromQualifiedString(sym.qualifiedName)
   }
 
   def fromQualifiedString(name: String): UseTree = {
