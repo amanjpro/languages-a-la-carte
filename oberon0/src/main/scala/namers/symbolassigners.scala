a9c688c90e5ef2ea6b728f0b6d4163d8478c68b0
amanjpro <amanj.sherwany@usi.ch>
2016-05-10 15:29:16 +0200

    Finish implementing symbolassigners

--- oberon0/src/main/scala/namers/symbolassigners.scala
+++ oberon0/src/main/scala/namers/symbolassigners.scala
@@ -10,7 +10,7 @@ import sana.oberon0
 
 import tiny.dsl._
 import oberon0.ast._
-import tiny.ast.{DefTree, UseTree}
+import tiny.ast.{DefTree, UseTree, Expr, IdentApi}
 import tiny.names.Name
 import tiny.source.Position
 import tiny.symbols.{Symbol, TypeSymbol}
@@ -18,12 +18,31 @@ import ooj.symbols.ClassSymbol
 import ooj.ast.{ClassDefApi, TemplateApi}
 import tiny.errors.ErrorReporting.{error, warning}
 import oberon0.errors.ErrorCodes._
-import primj.ast.BlockApi
+import primj.ast.{BlockApi, ValDefApi}
 import oberon0.ast.Implicits._
 import oberon0.symbols._
 import primj.namers.SymbolAssignerComponent
 
-
+// Program: DONE
+// ModuleDef: DONE
+// ClassDef: DONE
+// Template: DONE
+// TypeDef: DONE
+// TypeUse: DONE
+// ArrayTypeUse: DONE
+// Ident: DONE
+// MethodDef: DONE
+// ValDef: DONE
+// Apply: DONE
+// If: DONE
+// While: DONE
+// Block: DONE
+// Assign: DONE
+// ArrayAccess: DONE
+// Select: DONE
+// Binary: DONE
+// Unary: DONE
+// Literal: DONE
 
 
 @component
@@ -59,13 +78,10 @@ trait ModuleDefSymbolAssignerComponent extends SymbolAssignerComponent {
 trait ClassDefSymbolAssignerComponent
     extends ooj.namers.ClassDefSymbolAssignerComponent {
   override protected def classDoubleDefCheck(owner: Option[Symbol],
-    name: Name, pos: Option[Position]): Unit = owner match {
-    case Some(owner) if owner.directlyDefinesName(name,
-                               _.isInstanceOf[TypeSymbol])      =>
-      error(TYPE_ALREADY_DEFINED,
-          "", "", pos)
-    case _                                                      =>
-      ()
+    name: Name, pos: Option[Position]): Unit = {
+    // TypeDef must be unique but not ClassDef. ClassDef represents
+    // name-less records
+    ()
   }
 
   override protected def addDefaultConstructor(clazz: ClassDefApi,
@@ -76,7 +92,7 @@ trait ClassDefSymbolAssignerComponent
 
 
 @component
-trait TypeDefSymbolAssigner
+trait TypeDefSymbolAssignerComponent
     extends SymbolAssignerComponent {
 
   (tdef: TypeDefApi) => {
@@ -85,7 +101,8 @@ trait TypeDefSymbolAssigner
     val symbol = TypeDefSymbol(tdef.name, None, owner)
     tdef.symbol = symbol
     owner.foreach(tdef.tpt.owner = _)
-    val tpt    = assign(tdef.tpt).asInstanceOf[UseTree]
+    owner.foreach(_.declare(symbol))
+    val tpt    = assign(tdef.tpt)
     TreeCopiers.copyTypeDef(tdef)(tpt = tpt)
   }
 
@@ -97,5 +114,43 @@ trait TypeDefSymbolAssigner
           "", "", pos)
     case _                                                      =>
       ()
+  }
+}
+
+@component
+trait ArrayTypeUseSymbolAssignerComponent extends SymbolAssignerComponent {
+  (tuse: ArrayTypeUseApi)     => {
+    val owner = tuse.owner
+    owner.foreach { owner =>
+      tuse.tpt.owner = owner
+    }
+
+    val tpt  = assign(tuse.tpt).asInstanceOf[UseTree]
+    val size = assign(tuse.size).asInstanceOf[Expr]
+    TreeCopiers.copyArrayTypeUse(tuse)(tpt = tpt, size = size)
+  }
+
+}
+
+@component
+trait ValDefSymbolAssignerComponent
+  extends primj.namers.ValDefSymbolAssignerComponent {
+
+  override protected def declareSymbol(valdef: ValDefApi,
+    symbol: Symbol): Unit = valdef.owner.foreach(sym => {
+        sym.declare(symbol)
+      })
+}
+
+
+@component
+trait IdentSymbolAssignerComponent
+  extends primj.namers.IdentSymbolAssignerComponent {
+  (id: IdentApi)          => {
+    if(id.name == Name("WriteLn")) {
+      val tree = TreeFactories.mkApply(id, Nil, id.pos)
+      id.owner.foreach(tree.owner = _)
+      tree
+    } else super.apply(id)
   }
 }
