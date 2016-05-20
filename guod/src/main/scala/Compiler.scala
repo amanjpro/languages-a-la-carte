0d568519088855e0e1010a4753cb28f843d1e3d9
amanjpro <amanj.sherwany@usi.ch>
2016-05-20 11:11:08 +0200

    Add Compiler and Main for guod

---
+++ guod/src/main/scala/Compiler.scala
@@ -0,0 +1,68 @@
+package ch.usi.inf.l3.sana.guod
+
+import ch.usi.inf.l3.sana
+import sana.modulej
+import sana.guod
+import sana.tiny
+import guod.ast._
+import guod.phases._
+import modulej.phases._
+import tiny.errors.ErrorReporting
+import tiny.core.Implicits._
+import tiny.settings.SanaConfig
+import guod.codegen.{Env, ByteCodeWriter}
+
+trait Compiler extends modulej.Compiler {
+  self =>
+
+
+  override protected val language = new Language
+  class Language extends super.Language {
+
+
+    protected val qualifiers     = QualifierFamily(compiler).fullyqualify
+    protected val localvariables =
+      (t: Tree) => LocalVariablesFamily(compiler).subst((t, new Env))
+    protected val initializers   = InitializersFamily(compiler).inline
+    protected val generate       =
+      (t: Tree) => CodeGenFamily(compiler).codegen((t,
+        ByteCodeWriter("")))
+    def codegen: Tree => Unit = (t: Tree) => {
+      t match {
+        // case p               if !ErrorReporting.isErroneous             =>
+          // ()
+        case p                                                          =>
+          val f = initializers join
+                    qualifiers join  
+                      localvariables join
+                        generate
+          f(p)
+        // case _                                                          =>
+          // ()
+      }
+    }
+
+
+    override def compile: Tree => Unit = {
+      (x: Tree) => {
+
+        val f = symassigner.assign join
+                  namer.name join
+                    deftyper.typed join
+                      constantFolder join
+                        typer.typed join
+                          ShapeCheckerFamily(compiler).check join
+                            labelChecker join
+                              jumpChecker join
+                                forwardRefChecker join
+                                  constructorsChecker join
+                                    flowAnalyzer join
+                                      exceptionHandlingChecker join
+                                        codegen
+        f(x)
+      }
+    }
+  }
+}
+
+class CompilerImpl(val config: SanaConfig) extends Compiler
---
+++ guod/src/main/scala/Main.scala
@@ -0,0 +1,50 @@
+package ch.usi.inf.l3.sana.guod
+
+import ch.usi.inf.l3.sana
+import sana.tiny
+import sana.modulej.{langName, langVersion}
+import tiny.settings.{SanaConfig,CommandLineArgumentParser}
+import tiny.errors.ErrorReporting
+import tiny.debug.logger
+
+object Main {
+  def processOptions(args: Array[String],
+                        ln: String,
+                        lv: String,
+                        fn: String): Either[String, SanaConfig] = {
+        val config = new SanaConfig
+        val processor = new CommandLineArgumentParser(config, ln, lv, fn)
+        if(processor.parser.parse(args)) {
+          Right(config)
+        } else {
+          Left("Bad set of arguments\n" ++ processor.parser.usage)
+        }
+      }
+
+  def main(args: Array[String]): Unit = {
+
+
+    val config = processOptions(args, langName, langVersion,
+        tiny.frameworkName) match {
+      case Right(config) => config
+      case Left(msg)     =>
+        println(msg)
+        System.exit(1)
+        ???  // To satisfy the type checker
+    }
+
+    val ln = langName
+    val lv = langVersion
+    logger.setLevel(config.logLevel)
+
+    val compiler = new CompilerImpl(config)
+    compiler.start
+    ErrorReporting.isErroneous match {
+      case true     =>
+        ErrorReporting.errors.foreach(Console.err.println(_))
+        println(ErrorReporting.errors.length + " errors found")
+      case _        =>
+        println("Compilation Successful")
+    }
+  }
+}
